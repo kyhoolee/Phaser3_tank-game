0e2e06c8add9842e859f95e19a30e32ee8993fbf
Robert Kossessa <github@kosro.de>
2022-09-14 14:40:11 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import Phaser from 'phaser';
+import spritesTexture from './assets/allSprites_retina.png';
+import spritesAtlas from './assets/allSprites_retina.xml';
+
+
+class MyGame extends Phaser.Scene {
+    constructor() {
+        super()
+    }
+
+    preload() {
+        this.load.atlasXML('sprites', spritesTexture, spritesAtlas);
+    }
+
+    create() {
+        const atlasTexture = this.textures.get('sprites');
+
+        const frames = atlasTexture.getFrameNames();
+
+        for (let i = 0; i < frames.length; i++) {
+            const x = Phaser.Math.Between(0, 800);
+            const y = Phaser.Math.Between(0, 600);
+
+            this.add.image(x, y, 'sprites', frames[i]);
+        }
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: 'phaser-example',
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+const game = new Phaser.Game(config);
